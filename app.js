a8cbd4f757a90237dcc65193e644365bce56d2fb
Lucia <luciabide9@gmail.com>
2023-06-23 19:49:29 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,58 @@
+console.log("hola mundo!!")
+
+var string="mi primer string"
+var numero= 20
+var booleano= true
+
+console.log(string)
+console.log(numero)
+console.log(booleano)
+
+var num1= 2
+var num2= 2
+var suma= num1+num2
+console.log(suma)
+
+console.log("Operador")
+var text1 = "hola"
+var text2 = "hola"
+var text3 = "chau"
+
+console.log("Operador ==")
+console.log(text1==text2) //true
+
+console.log("Operador ==")
+console.log(text1 == text3) //false
+
+console.log("Operador de condicion")
+var condicion1 = true
+var condicion2 = true
+var condicion3 = false
+var condicion4 = false
+
+console.log("Operador &&")
+console.log(condicion1 && condicion2) //true
+console.log(condicion1 && condicion3) //false
+
+console.log("Condicion: IF ELSE")
+
+console.log("condicion1")
+if (condicion1) {
+    console.log("Resultado A")
+} else {
+    console.log("Resultado B")
+} //resultado a
+
+console.log("condicion3")
+if (condicion3) {
+    console.log("Resultado A")
+} else {
+    console.log("Resultado B")
+} //resultado b
+
+console.log("condicion1 && condicion3")
+if (condicion3 && condicion1) {
+    console.log("Resultado A")
+} else {
+    console.log("Resultado B")
+} //resultado b
